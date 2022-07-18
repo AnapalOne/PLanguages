79a05b8533d7d653e0b1a893f8666b0d2327b737
AnapalOne <andreijosee@zoho.com>
2022-07-18 13:48:51 +0800

    Learning C#

---
+++ C#/How to/HelloWorld.cs
@@ -0,0 +1,9 @@
+using System;
+
+namepsace helloWorld {
+    class helloCSharp {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ C#/Programs/BasicOperations.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace calculator {
+    class calculatorExample {
+        public static void Main(string[] args) {
+            int sum;
+            int a = 20;
+            int b = 5;
+            sum = a + b;
+            Console.WriteLine("The sum of these two numbers are: {0}", sum);
+        }
+    }
+}
