ff65bc9a233cfc6a17f3b698db1e4f9c3cfb47bc
AnapalOne <andreijosee@zoho.com>
2022-07-18 14:02:47 +0800

    Updated .gitignore

--- C++/tempCodeRunnerFile.cpp
+++ C++/tempCodeRunnerFile.cpp
@@ -1,19 +1,5 @@
 #include <iostream>
-using namespace std;
 
-float process(float a,float b) {
-
-    int t = 0;
-    t = a + b;
-    return t;
-}
-
-main () {
-
-    cout << process(5,6.5);
-    
-}
-
-string run(int index) {
-    return 0;
+main() {
+    std::cout << "Hello world\n";
 }
