58159f59c9973ace507e5777702517f7d1926469
AnapalOne <andreijosee@zoho.com>
2022-07-26 13:03:22 +0800

    Update FibinachiSeq.cpp

--- C++/Programs/FibinachiSeq.cpp
+++ C++/Programs/FibinachiSeq.cpp
@@ -5,30 +5,30 @@ int main() {
     cout << "this program fibinachi whatever spelling idc\n";
     cout << "factors in the fibinacci sequence: ";
     
-    int fibseq1 = 0, n, x1 = 0, y1 = 1, temp1 = 0;
+    int n, x1 = 0, y1 = 1, z1;
         //for fibinachi
     cin >> n;
     cout << "Sequence: ";
    for(int i = 1;i <= n; ++i) { 
-        cout << fibseq1 << " ";
-        fibseq1 = x1 + y1;
-        temp1 = x1;
-        y1 = temp1;
-        x1 = fibseq1;
+        cout << x1 << " ";
 
+        z1 = x1 + y1;
+        x1 = y1;
+        y1 = z1;
    }
 
-    int fibseq2 = 0, x2 = 0, y2 = 1, z = 1, temp2 = 0;
+    int z = 0, x2 = 0, y2 = 1, z2;
         //while fibinachi 
     cout << endl << "factors in the fibinacci sequence: ";
     cin >> n;
     cout << "Sequence: ";
-    while(z <= n) {
-        cout << fibseq2 << " ";
-        fibseq2 = x2 + y2;
-        temp2 = x2;
-        y2 = temp2;
-        x2 = fibseq2;
+    while(z < n) {
+        cout << z2 << " ";
+
+        z2 = x2 + y2;
+        y2 = x2;
+        x2 = z2;
+
         z++;
     }
 }
